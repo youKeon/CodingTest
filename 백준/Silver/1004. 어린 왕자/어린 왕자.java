5447d9e28fe22b334f1c2271f2dbb78482f2da18
youKeon <yukeon97@gmail.com>
2024-03-19 22:57:51 +0900

    [Silver III] Title: 어린 왕자, Time: 164 ms, Memory: 15408 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.java
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.java
@@ -6,26 +6,27 @@ class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+        StringTokenizer st = null;
 
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
-            int startX = Integer.parseInt(st.nextToken());
-            int startY = Integer.parseInt(st.nextToken());
-            int endX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
+            int fromX = Integer.parseInt(st.nextToken());
+            int fromY = Integer.parseInt(st.nextToken());
+            int toX = Integer.parseInt(st.nextToken());
+            int toY = Integer.parseInt(st.nextToken());
 
-            int n = Integer.parseInt(br.readLine());
             int ans = 0;
+            int n = Integer.parseInt(br.readLine());
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
+                int cx = Integer.parseInt(st.nextToken());
+                int cy = Integer.parseInt(st.nextToken());
                 int r = Integer.parseInt(st.nextToken());
-                boolean start = check(startX, startY, x, y, r);
-                boolean end = check(endX, endY, x, y, r);
-                if (start != end) ans++;
+
+                boolean result1 = isIncluded(fromX, fromY, cx, cy, r);
+                boolean result2 = isIncluded(toX, toY, cx, cy, r);
+                if (result1 != result2) ans++;
             }
             bw.write(String.valueOf(ans));
             bw.newLine();
@@ -34,10 +35,8 @@ class Main {
         br.close();
     }
 
-    private static boolean check(int startX, int startY, int x, int y, int r) {
-        int distance = (startX - x) * (startX - x) + (startY - y) * (startY - y);
-        return r * r >= distance;
+    private static boolean isIncluded(int x, int y, int cx, int cy, int r) {
+        int dis = (x - cx) * (x - cx) + (y - cy) * (y - cy);
+        return dis <= r * r;
     }
-
-
 }
