bcf2689a53f5a7daecd01f4219337045e38c965a
youKeon <yukeon97@gmail.com>
2024-03-28 23:01:38 +0900

    [Silver III] Title: N과 M (4), Time: 156 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,37 +2,35 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int n, m;
-    static int[] ans, arr;
-    static StringBuilder sb = new StringBuilder();
-    
-    static void dfs(int depth, int at) {
-        if (depth == m) {
-            for (int i : ans) sb.append(i).append(" ");
+    static int N, M;
+    static int[] arr;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int depth, int pre) {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
             sb.append("\n");
             return;
         }
-        
-        for (int i = at; i <= n; i++) {
-            ans[depth] = i;
+
+        for (int i = pre; i < N; i++) {
+            arr[depth] = i + 1;
             dfs(depth + 1, i);
         }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        
-        ans = new int[m];
-        
-        dfs(0, 1);
-        
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
     }
 }
