0a262a9f9cfa27baa834ce371fb62a3addefc70c
youKeon <yukeon97@gmail.com>
2024-04-01 22:42:25 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,24 +1,22 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int ans = 0;
-        int sum = 0;
-
         String[] arr = br.readLine().split("-");
+        int ans = 0;
+
         for (int i = 0; i < arr.length; i++) {
-            sum = 0;
-            String[] split = arr[i].split("\\+");
-            for (String s : split) {
-                sum += Integer.parseInt(s);
+            String[] tmp = arr[i].split("\\+");
+            int total = 0;
+            for (String s : tmp) {
+                total += Integer.parseInt(s);
             }
-            if (i == 0) ans += sum;
-            else ans -= sum;
+
+            if (i == 0) ans += total;
+            else ans -= total;
         }
-        
-        System.out.print(ans);
+        System.out.println(ans);
     }
 }
