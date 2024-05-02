fae2c3fe866915d3c3a9d1938e6ecb789888fd0a
youKeon <yukeon97@gmail.com>
2024-05-02 23:59:31 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 22056 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        char s[] = br.readLine().toCharArray();
+
+        int result = 0;
+        int count = 0;
+
+        for(int i=1; i < M - 1; i++) {
+            if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+                count++;
+
+                if(count == N) {
+                    count--;
+                    result++;
+                }
+                i++;
+            }
+            else {
+                count = 0;
+            }
+        }
+
+        System.out.print(result);
+    }
+}
