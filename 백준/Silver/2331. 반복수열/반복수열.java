83be32ce95c0144d04b7ebef6023b34db546e629
youKeon <yukeon97@gmail.com>
2024-05-06 19:20:24 +0900

    [Silver IV] Title: 반복수열, Time: 124 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.java
+++ 백준/Silver/2331. 반복수열/반복수열.java
@@ -2,37 +2,32 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<>();
         int A = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
         int ans = 0;
+
+        List<Integer> list = new ArrayList<>();
         list.add(A);
 
         while (true) {
             Integer cur = list.get(list.size() - 1);
-            int tmp = operation(cur, P);
-
-            if (list.contains(tmp)) {
-                ans = list.indexOf(tmp);
+            int sum = 0;
+            while (cur > 0) {
+                int remain = cur % 10;
+                sum += Math.pow(remain, P);
+                cur /= 10;
+            }
+            if (list.contains(sum)) {
+                ans = list.indexOf(sum);
                 break;
             }
-
-            list.add(tmp);
+            list.add(sum);
         }
         System.out.print(ans);
-    }
-
-    private static int operation(Integer cur, int p) {
-        int result = 0;
-        while (cur != 0) {
-            int tmp = cur % 10;
-            result += Math.pow(tmp, p);
-            cur /= 10;
-        }
-        return result;
     }
 }
