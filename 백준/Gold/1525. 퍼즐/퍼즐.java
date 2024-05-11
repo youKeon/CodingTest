a8d5135750f474ef59d5cbe5d8106cdb87336d7c
youKeon <yukeon97@gmail.com>
2024-05-11 14:33:25 +0900

    [Gold II] Title: 퍼즐, Time: 816 ms, Memory: 115252 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -1,57 +1,53 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    static final String ANS = "123456780";
-    static Map<String, Integer> hashMap = new HashMap();
-    static int[][] map;
+    static final String ANSWER = "123456780";
+    static Map<String, Integer> isVisited = new HashMap<>();
+    static String[][] map;
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        map = new int[9][9];
+        map = new String[9][9];
         String init = "";
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = st.nextToken();
                 init += map[i][j];
             }
         }
 
-        int count = bfs(init);
-        System.out.print(count);
+        int result = bfs(init);
+        System.out.print(result);
     }
 
     private static int bfs(String init) {
         Queue<String> q = new LinkedList<>();
+        isVisited.put(init, 0);
         q.add(init);
-        hashMap.put(init, 0);
 
         while (!q.isEmpty()) {
             String cur = q.poll();
-            if (cur.equals(ANS)) {
-                return hashMap.get(cur);
-            }
-
-            int zeroIdx = cur.indexOf("0");
-            int y = zeroIdx / 3;
-            int x = zeroIdx % 3;
+            if (cur.equals(ANSWER)) return isVisited.get(cur);
+            int indexOfZero = cur.indexOf("0");
+            int y = indexOfZero / 3;
+            int x = indexOfZero % 3;
 
             for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
 
-                if (isValidZone(ny, nx)) {
-                    int nextIdx = (ny * 3) + nx;
-                    String nextString = swap(cur, nextIdx);
+                if (isValid(ny, nx)) {
+                    String nextString = swap(cur, ny, nx);
 
-                    if (!hashMap.containsKey(nextString)) {
-                        hashMap.put(nextString, hashMap.get(cur) + 1);
+                    if (!isVisited.containsKey(nextString)) {
                         q.add(nextString);
+                        isVisited.put(nextString, isVisited.get(cur) + 1);
                     }
                 }
             }
@@ -59,15 +55,15 @@ class Main {
         return -1;
     }
 
-    private static String swap(String cur, int nextIdx) {
-        char target = cur.charAt(nextIdx);
+    private static String swap(String cur, int ny, int nx) {
+        char target = cur.charAt((ny * 3) + nx);
         String nextString = cur.replace(target, '-');
         nextString = nextString.replace('0', target);
         nextString = nextString.replace('-', '0');
         return nextString;
     }
 
-    private static boolean isValidZone(int ny, int nx) {
+    private static boolean isValid(int ny, int nx) {
         return ny >= 0 && ny < 3 && nx >= 0 && nx < 3;
     }
 }
