b4adb2ecf76ee9ea07c0fab7cbbc19016e5a3076
youKeon <yukeon97@gmail.com>
2024-05-14 22:30:57 +0900

    [Silver V] Title: 올림픽, Time: 148 ms, Memory: 14684 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -6,27 +6,35 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        Map<Integer, Integer> map = new HashMap<>();
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); // 국가 수
         int K = Integer.parseInt(st.nextToken()); // 등수를 알고 싶은 국가
+        int[][] arr = new int[N][3];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int country = Integer.parseInt(st.nextToken());
-            int gold = Integer.parseInt(st.nextToken());
-            int silver = Integer.parseInt(st.nextToken());
-            int bronze = Integer.parseInt(st.nextToken());
-
-            map.put(country, (gold * 3) + (silver * 2) + bronze);
+            arr[country - 1][0] = Integer.parseInt(st.nextToken());
+            arr[country - 1][1] = Integer.parseInt(st.nextToken());
+            arr[country - 1][2] = Integer.parseInt(st.nextToken());
         }
+        int[] target = arr[K - 1];
 
-        int ans = 1;
-        int target = map.get(K);
-        for (Integer i : map.keySet()) {
-            if (map.get(i) > target) ans++;
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                if (o1[1] == o2[1]) {
+                    return o2[2] - o1[2];
+                } else return o2[1] - o1[1];
+            } else return o2[0] - o1[0];
+        });
+
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (Arrays.equals(arr[i], target)) {
+                ans = i + 1;
+                break;
+            }
         }
-
         System.out.print(ans);
     }
 }
