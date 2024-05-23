28c0cd69fbf7781d1954b3a9c8ea60127d4b33ee
youKeon <yukeon97@gmail.com>
2024-05-23 22:19:55 +0900

    [Silver II] Title: 비슷한 단어, Time: 128 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
+        int[] word = new int[26];
+
+        String og = br.readLine();
+        for (int i = 0; i < og.length(); i++) {
+            word[og.charAt(i) - 'A']++;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int[] copy = word.clone();
+            int count = 0;
+            String comp = br.readLine();
+
+            for (int j = 0; j < comp.length(); j++) {
+                if (copy[comp.charAt(j) - 'A'] > 0) {
+                    copy[comp.charAt(j) - 'A']--;
+                    count++;
+                }
+            }
+
+            if (og.length() == comp.length() && (og.length() == count || og.length() - 1 == count)) {
+                ans++;
+            } else if (og.length() == comp.length() - 1 && comp.length() - 1 == count) {
+                ans++;
+            } else if (og.length() == comp.length() + 1 && comp.length() == count) {
+                ans++;
+            }
+        }
+        System.out.print(ans);
+    }
+}
