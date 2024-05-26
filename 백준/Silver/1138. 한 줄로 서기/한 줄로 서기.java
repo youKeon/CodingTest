68aa595a05c035a5bd1352fcec92c68d8c412d82
youKeon <yukeon97@gmail.com>
2024-05-27 01:02:12 +0900

    [Silver II] Title: 한 줄로 서기, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -1,26 +1,26 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = null;
+        List<Integer> ans = new ArrayList<>();
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n + 1];
-        List<Integer> list = new ArrayList<>();
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = n; i >= 1; i--) {
-            list.add(arr[i], i);
+            ans.add(arr[i], i);
         }
 
-        for (Integer i : list) {
+        for (Integer i : ans) {
             bw.write(i + " ");
         }
         bw.close();
