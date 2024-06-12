eecc581134e1623390658428808f6a7360331c19
youKeon <yukeon97@gmail.com>
2024-06-13 00:58:21 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,16 +1,12 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution {
+    public int solution(int n, int a, int b) {
         int ans = 0;
 
         while(a != b) {
             a = (a / 2) + (a % 2); 
             b = (b / 2) + (b % 2);
-
             ans++; 
         }
-
         return ans;
     }
 }
