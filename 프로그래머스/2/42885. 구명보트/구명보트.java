866a529e385dc158d88192e30445f0bd7bd1f972
youKeon <yukeon97@gmail.com>
2024-06-15 13:14:09 +0900

    [level 2] Title: 구명보트, Time: 8.49 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -6,15 +6,18 @@ class Solution {
         int ans = 0;
         int p1 = 0;
         int p2 = people.length - 1;
+        int total = 0;
         
         while (p1 <= p2) {
-            if (people[p1] + people[p2] <= limit) {
+            total = people[p1] + people[p2];
+            
+            if (total > limit) {
+                ans++;
+                p2--;
+            } else if (total <= limit) {
+                ans++;
+                p2--;
                 p1++;
-                p2--;
-                ans++;
-            } else {
-                p2--;
-                ans++;
             }
         }
         return ans;
