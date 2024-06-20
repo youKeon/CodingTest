a3d651f21b2495a703bcb0559e098755725b213a
youKeon <yukeon97@gmail.com>
2024-06-20 13:01:32 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,26 @@
 class Solution {
-    int[][] map;
     boolean[] isVisited;
-    int n, ans;
-    
     public int solution(int n, int[][] computers) {
-        this.map = computers;
+        int ans = 0;
         this.isVisited = new boolean[n];
-        this.n = n;
-        this.ans = 0;
         
         for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
                 isVisited[i] = true;
                 ans++;
-                dfs(i);
+                
+                dfs(n, computers, i);
             }
         }
         return ans;
     }
     
-    private void dfs(int l) {
+    private void dfs(int n, int[][] computers, int l) {
         for (int i = 0; i < n; i++) {
-            if (!isVisited[i] && map[l][i] == 1) {
+            if (!isVisited[i] && computers[l][i] == 1) {
                 isVisited[i] = true;
-                dfs(i);
+                dfs(n, computers, i);
             }
         }
     }
 }
-
-// 1 1 0
-// 1 1 0
-// 0 0 1
