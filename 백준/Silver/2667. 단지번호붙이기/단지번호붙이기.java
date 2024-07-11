c3ffa9c5bb98502d9c34d94594d412dc856cf412
youKeon <yukeon97@gmail.com>
2024-07-11 20:17:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,72 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
     static final int MAX = 30;
-    static int n, cnt, range;
-    static boolean[][] map;
-    static int[] ans;
-    static int[] dx = {0, 0, -1, 1};
+    static int N;
+    static int[][] map;
     static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
 
-    static void dfs(int y, int x) {
-        map[y][x] = false;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (map[ny][nx]) {
-                range++;
-                dfs(ny, nx);
-            }
+    static class Node {
+        int y;
+        int x;
+
+        public Node(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
     }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+        List<Integer> ans = new ArrayList<>();
 
-        n = Integer.parseInt(br.readLine());
-        map = new boolean[MAX][MAX];
-        ans = new int[MAX];
-        ArrayList<Integer> arr = new ArrayList();
+        N = Integer.parseInt(br.readLine());
+        map = new int[MAX][MAX];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < N; i++) {
             String tmp = br.readLine();
-            for (int j = 0; j < n; j++) {
-                map[i][j+1] = (tmp.charAt(j) == '1') ? true : false;
+            for (int j = 0; j < N; j++) {
+                map[i + 1][j + 1] = tmp.charAt(j) - '0';
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (map[i][j]) {
-                    cnt++;
-                    range = 1;
-                    dfs(i, j);
-                    arr.add(range);
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (map[i][j] == 1) {
+                    int size = bfs(i, j);
+                    ans.add(size);
                 }
             }
         }
-
-        Collections.sort(arr);
-
-        bw.write(String.valueOf(cnt));
-        bw.newLine();
-        for (int i : arr) {
-            bw.write(String.valueOf(i));
-            bw.newLine();
+        ans.sort(Comparator.naturalOrder());
+        System.out.println(ans.size());
+        for (Integer i : ans) {
+            System.out.println(i);
         }
+    }
 
-        bw.close();
-        br.close();
+    private static int bfs(int y, int x) {
+        Deque<Node> dq = new ArrayDeque<>();
+        dq.offer(new Node(y, x));
+        map[y][x] = 0;
+        int result = 1;
+
+        while (!dq.isEmpty()) {
+            Node cur = dq.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = cur.y + dy[i];
+                int nx = cur.x + dx[i];
+
+                if (map[ny][nx] == 1) {
+                    dq.offer(new Node(ny, nx));
+                    map[ny][nx] = 0;
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 }
