1264ada21a8a1b8271b790850f709cc27ef4c4e2
youKeon <yukeon97@gmail.com>
2024-07-18 22:14:20 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 788 ms, Memory: 111308 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,76 +1,72 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static class Point {
-        int x, y, dist, wallBroken;
-
-        Point(int x, int y, int dist, int wallBroken) {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-            this.wallBroken = wallBroken;
-        }
-    }
-
+class Main {
     static int N, M;
     static int[][] map;
-    static boolean[][][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static boolean[][][] isVisited;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    static class Node {
+        int y, x, dist, isBroke;
+
+        public Node(int y, int x, int dist, int isBroke) {
+            this.y = y;
+            this.x = x;
+            this.dist = dist;
+            this.isBroke = isBroke;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
 
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M][2];
 
+        map = new int[N + 1][M + 1]; 
+        isVisited = new boolean[N + 1][M + 1][2];
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
+            String tmp = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = line.charAt(j) - '0';
+                map[i][j] = tmp.charAt(j) - '0';
             }
         }
 
-        int result = bfs();
-        System.out.println(result);
+        int ans = bfs();
+        System.out.print(ans);
     }
 
     private static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0, 1, 0));
-        visited[0][0][0] = true;
+        Deque<Node> dq = new ArrayDeque<>();
+        dq.offer(new Node(0, 0, 1, 0));
+        isVisited[0][0][0] = true;
 
-        while (!queue.isEmpty()) {
-            Point current = queue.poll();
+        while (!dq.isEmpty()) {
+            Node cur = dq.poll();
 
-            if (current.x == N - 1 && current.y == M - 1) {
-                return current.dist;
+            if (cur.y == N - 1 && cur.x == M - 1) {
+                return cur.dist;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
+                int ny = cur.y + dy[i];
+                int nx = cur.x + dx[i];
 
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                    if (map[nx][ny] == 0) {
-                        if (!visited[nx][ny][current.wallBroken]) {
-                            queue.offer(new Point(nx, ny, current.dist + 1, current.wallBroken));
-                            visited[nx][ny][current.wallBroken] = true;
-                        }
-                    } else if (map[nx][ny] == 1 && current.wallBroken == 0) {
-                        if (!visited[nx][ny][1]) {
-                            queue.offer(new Point(nx, ny, current.dist + 1, 1));
-                            visited[nx][ny][1] = true;
-                        }
+                if (ny >= 0 && ny < N && nx >= 0 && nx < M) { 
+                    if (map[ny][nx] == 0 && !isVisited[ny][nx][cur.isBroke]) {
+                        dq.offer(new Node(ny, nx, cur.dist + 1, cur.isBroke));
+                        isVisited[ny][nx][cur.isBroke] = true;
+                    } else if (map[ny][nx] == 1 && cur.isBroke == 0 && !isVisited[ny][nx][1]) {
+                        dq.offer(new Node(ny, nx, cur.dist + 1, 1));
+                        isVisited[ny][nx][1] = true;
                     }
                 }
             }
         }
-
         return -1;
     }
 }
