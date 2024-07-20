fd25a6fe871744d23f70302af584c4eaf788b962
youKeon <yukeon97@gmail.com>
2024-07-20 18:27:29 +0900

    [Silver V] Title: 돌 게임, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -1,14 +1,25 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        if (n % 2 == 1) {
-            System.out.println("SK");
-        } else {
-            System.out.println("CY");
+        int[] dp = new int[n + 1];
+
+        if (n == 1) System.out.print("SK");
+        else if (n == 2) System.out.print("CY");
+        else if (n == 3) System.out.print("SK");
+        else if (n == 4) System.out.print("CY");
+        else {
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 1;
+            for (int i = 4; i <= n; i++) {
+                dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+            }
+            if (dp[n] % 2 == 0) System.out.print("CY");
+            else System.out.print("SK");
         }
     }
 }
