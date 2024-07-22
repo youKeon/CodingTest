37f159afd4446b43986d6fc65d6aa11d7c748ea0
youKeon <yukeon97@gmail.com>
2024-07-22 20:55:12 +0900

    [Silver III] Title: Four Squares, Time: 144 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -1,29 +1,20 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[N + 1];
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
         dp[1] = 1;
 
-        int min = 0;
-        for (int i = 2; i <= N; i++) {
-            min = Integer.MAX_VALUE;
-
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j * j <= i; j++) {
-                int tmp = i - j * j;
-                min = Math.min(min, dp[tmp]);
+                dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
             }
-
-            dp[i] = min + 1;
         }
-        bw.write(dp[N] + "\n");
-        bw.close();
-        br.close();    
+        System.out.print(dp[n]);
     }
 }
