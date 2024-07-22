318eed5eae1babe783bfeb76e737206f3167b8cf
youKeon <yukeon97@gmail.com>
2024-07-22 21:43:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,34 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
-    static int n, ans;
-
-    public static void main(String[] args) throws IOException{
+    static int[] arr = {1, 2, 3};
+    static int ans;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
         int tc = Integer.parseInt(br.readLine());
-
         while (tc-- > 0) {
-            n = Integer.parseInt(br.readLine());
-            ans = 0;
-            dfs(0, n);
-            bw.write(String.valueOf(ans));
-            bw.newLine();
-        }
-        bw.close();
-        br.close();
-    }
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) System.out.println(0);
+            else if (n == 1) System.out.println(1);
+            else if (n == 2) System.out.println(2);
+            else if (n == 3) System.out.println(4);
+            else {
+                int[] dp = new int[n + 1];
+                dp[0] = 0;
+                dp[1] = 1;
+                dp[2] = 2;
+                dp[3] = 4;
 
-    private static void dfs(int total, int l) {
-        if (total >= n) {
-            if (total == n) {
-                ans++;
+                for (int i = 4; i <= n; i++) {
+                    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+                }
+                System.out.println(dp[n]);
             }
-            return;
         }
-
-        dfs(total + 1, l + 1);
-        dfs(total + 2, l + 1);
-        dfs(total + 3, l + 1);
     }
 }
