ddd0737851fb616e136b239e84b701bf51a1d99a
youKeon <yukeon97@gmail.com>
2024-07-28 00:27:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,30 @@
-import java.util.*;
 import java.io.*;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        int ans = 0;
 
-        while (n != 0) {
-            if (n % 5 == 0) {
-                ans = n / 5 + ans;
-                break;
-            } else if (n < 0) {
-                ans = -1;
-                break;
-            }
-            ans++;
-            n -= 3;
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(solution(n));
+    }
+
+    private static int solution(int n) {
+        if (n == 3) return 1;
+        if (n == 4) return -1;
+        if (n == 5) return 1;
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[3] = 1;
+        dp[4] = -1;
+        dp[5] = 1;
+
+        for (int i = 6; i <= n; i++) {
+            if (dp[i - 3] != -1 && dp[i - 3] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i - 3]) + 1;
+            if (dp[i - 5] != -1 && dp[i - 5] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[i - 5]) + 1;
         }
-        bw.write(String.valueOf(ans));
-        bw.close();
-        br.close();
+
+        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
     }
 }
