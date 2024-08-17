2fd5c5b03cc1f36cc2fa83e3354f07612f704101
youKeon <yukeon97@gmail.com>
2024-08-17 20:35:51 +0900

    [Silver II] Title: 좌표 압축, Time: 1860 ms, Memory: 261620 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,33 +1,36 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
 
-        HashMap<Integer, Integer> map = new HashMap();
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
         int[] arr = new int[n];
+        int[] sortedArr = new int[n];
+        Map<Integer, Integer> map = new HashMap<>();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            sortedArr[i] = arr[i];
         }
+        Arrays.sort(sortedArr);
 
-        int[] tmp = arr.clone();
-
-        Arrays.sort(tmp);
-
-        for (int i = 0; i < arr.length; i++) {
-            if (!map.containsKey(tmp[i]))
-                map.put(tmp[i], cnt++);
+        int index = 0;
+        map.put(sortedArr[0], index++);
+        for (int i = 1; i < n; i++) {
+            if (sortedArr[i - 1] == sortedArr[i]) {
+                map.put(sortedArr[i], map.get(sortedArr[i - 1]));
+            } else {
+                map.put(sortedArr[i], index++);
+            }
         }
-        for (int i = 0; i < n; i++) {
-            bw.write(map.get(arr[i]) + " ");
+        for (int i : arr) {
+            sb.append(map.get(i)).append(" ");
         }
-        bw.close();
-        br.close();
+        System.out.print(sb);
     }
 }
