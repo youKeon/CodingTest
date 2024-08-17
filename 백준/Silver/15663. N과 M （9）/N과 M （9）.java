749270722e56f15058246242cf5f9b9da0fe40fd
youKeon <yukeon97@gmail.com>
2024-08-17 20:06:02 +0900

    [Silver II] Title: N과 M (9), Time: 168 ms, Memory: 21924 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,48 +1,52 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int n, m;
-    static boolean[] chk;
-    static int[] arr, tmp;
+    static int[] arr, ans;
+    static boolean[] isVisited;
+    static StringBuilder sb;
+    static int M, N;
 
-    static void dfs(int l) {
-        if (l == m) {
-            for (int i : tmp) sb.append(i).append(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        isVisited = new boolean[N];
+        sb = new StringBuilder();
+        arr = new int[N];
+        ans = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        dfs(0, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int l, int depth) {
+        if (depth == M) {
+            for (int i : ans) {
+                sb.append(i).append(" ");
+            }
             sb.append("\n");
             return;
         }
 
-        int pre = -1;
-        for (int i = 0; i < n; i++) {
-            if (!chk[i] && arr[i] != pre) {
-                chk[i] = true;
-                tmp[l] = arr[i];
+        int pre = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] != pre && !isVisited[i]) {
                 pre = arr[i];
-                dfs(l + 1);
-                chk[i] = false;
+                ans[depth] = arr[i];
+                isVisited[i] = true;
+                dfs(i, depth + 1);
+                isVisited[i] = false;
             }
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        chk = new boolean[n];
-        tmp = new int[m];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
-
-        dfs(0);
-
-        System.out.print(sb);
     }
 }
