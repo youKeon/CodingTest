e599a263340032a57029f26d96dc7448bc321a41
youKeon <yukeon97@gmail.com>
2024-08-17 21:15:05 +0900

    [Silver I] Title: 회의실 배정, Time: 452 ms, Memory: 44620 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,13 +1,12 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
+
         int n = Integer.parseInt(br.readLine());
-        int ans = 0;
         int[][] arr = new int[n][2];
 
         for (int i = 0; i < n; i++) {
@@ -17,15 +16,15 @@ class Main {
         }
 
         Arrays.sort(arr, (o1, o2) -> {
-            if (o1[1] == o2[1]) return o1[0] - o2[0];
-            return o1[1] - o2[1];
+            if (o1[1] != o2[1]) return o1[1] - o2[1];
+            return o1[0] - o2[0];
         });
 
-        int preEndTime = 0;
-        for (int[] ints : arr) {
-            int curStartTime = ints[0];
-            if (preEndTime <= curStartTime) {
-                preEndTime = ints[1];
+        int ans = 1;
+        int preEnd = arr[0][1];
+        for (int i = 1; i < n; i++) {
+            if (arr[i][0] >= preEnd) {
+                preEnd = arr[i][1];
                 ans++;
             }
         }
