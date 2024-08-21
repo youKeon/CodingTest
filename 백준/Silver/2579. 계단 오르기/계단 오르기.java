190c82cfc6828af787fcebc5b4f3a25052bdd188
youKeon <yukeon97@gmail.com>
2024-08-21 22:31:20 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,21 +7,25 @@ class Main {
         StringTokenizer st = null;
 
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
         int[] arr = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        if (n == 1) System.out.print(arr[1]);
-        else if (n == 2) System.out.print(arr[1] + arr[2]);
-        else {
-            dp[1] = arr[1];
-            dp[2] = arr[1] + arr[2];
-            for (int i = 3; i <= n; i++) {
-                dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
-            }
-            System.out.print(dp[n]);
+        long ans = solution(n, arr);
+        System.out.print(ans);
+    }
+
+    private static long solution(int n, int[] arr) {
+        if (n == 1) return arr[1];
+        if (n == 2) return arr[1] + arr[2];
+        long[] dp = new long[n + 1];
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
         }
+        return dp[n];
     }
 }
