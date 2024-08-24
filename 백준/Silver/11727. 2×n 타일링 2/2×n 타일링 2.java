e8a74a03154cdf9f922aaed7c584a8f94b48db13
youKeon <yukeon97@gmail.com>
2024-08-24 18:39:11 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 100 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -4,24 +4,21 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
 
         int n = Integer.parseInt(br.readLine());
-        System.out.print(solution(n));
+        System.out.println(solution(n));
     }
 
     private static int solution(int n) {
         if (n == 1) return 1;
-        if (n == 2) return 3;
-        if (n == 3) return 5;
-
         int[] dp = new int[n + 1];
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 3;
 
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 2]) % 10007;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * (dp[i - 2])) % 10007;
         }
+
         return dp[n];
     }
 }
