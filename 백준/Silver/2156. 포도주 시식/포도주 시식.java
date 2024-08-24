8e47108512e2bca0adacc54dfb5ad574ed42c49b
youKeon <yukeon97@gmail.com>
2024-08-25 00:28:13 +0900

    [Silver I] Title: 포도주 시식, Time: 132 ms, Memory: 15392 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -7,23 +7,27 @@ class Main {
         StringTokenizer st = null;
 
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        int[] arr = new int[n + 1];
+        int[] arr = new int[n];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
+        System.out.print(solution(n, arr));
+    }
 
-        if (n == 1) System.out.print(arr[1]);
-        else if (n == 2) System.out.print(arr[1] + arr[2]);
-        else {
-            dp[1] = arr[1];
-            dp[2] = arr[1] + arr[2];
+    private static int solution(int n, int[] arr) {
+        if (n == 1) return arr[0];
+        if (n == 2) return arr[0] + arr[1];
 
-            for (int i = 3; i <= n; i++) {
-                dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
-            }
-            System.out.println(dp[n]);
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        dp[1] = arr[0] + arr[1];
+        dp[2] = Math.max(dp[1], Math.max(arr[0], arr[1]) + arr[2]);
+
+        for (int i = 3; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i]);
         }
+
+        return Arrays.stream(dp).max().getAsInt();
     }
 }
