ae52d9be34a0f1b1a5e48918beccd7cf57701585
youKeon <yukeon97@gmail.com>
2024-08-24 15:58:02 +0900

    [Gold III] Title: 치즈, Time: 224 ms, Memory: 28912 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,82 +1,96 @@
+import java.io.*;
 import java.util.*;
 
 class Main {
     static int N, M;
-    static int[][] grid;
-    static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int[][] map;
+    static boolean[][] isVisited;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0,-1, 1};
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        N = scanner.nextInt();
-        M = scanner.nextInt();
-        grid = new int[N][M];
+    static class Node {
+        int y;
+        int x;
 
+        public Node(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                grid[i][j] = scanner.nextInt();
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int time = 0;
-        while (hasCheeseLeft()) {
-            simulateAir();
-            meltCheese();
-            time++;
+        int ans = 0;
+        while (hasCheese()) {
+            ans++;
+            bfs();
+            melt();
         }
-
-        System.out.println(time);
+        System.out.print(ans);
     }
 
-    static void simulateAir() {
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[N][M];
-        queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
+    private static void bfs() {
+        Deque<Node> dq = new ArrayDeque<>();
+        dq.offer(new Node(0, 0));
+        isVisited = new boolean[N][M];
+        isVisited[0][0] = true;
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            for (int[] dir : directions) {
-                int nx = current[0] + dir[0];
-                int ny = current[1] + dir[1];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && grid[nx][ny] <= 0) {
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    grid[nx][ny] = -1; 
+        while (!dq.isEmpty()) {
+            Node cur = dq.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int ny = cur.y + dy[i];
+                int nx = cur.x + dx[i];
+
+                if (ny >= 0 && ny < N && nx >= 0 && nx < M && !isVisited[ny][nx] && map[ny][nx] <= 0) {
+                    map[ny][nx] = -1;
+                    isVisited[ny][nx] = true;
+                    dq.offer(new Node(ny, nx));
                 }
             }
         }
     }
 
-    static void meltCheese() {
+    private static void melt() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (grid[i][j] == 1) {
-                    int airContact = 0;
-                    for (int[] dir : directions) {
-                        int nx = i + dir[0];
-                        int ny = j + dir[1];
-                        if (nx >= 0 && nx < N && ny >= 0 && ny < M && grid[nx][ny] == -1) {
-                            airContact++;
+                if (map[i][j] == 1) {
+                    int count = 0;
+                    for (int k = 0; k < 4; k++) {
+                        int ny = i + dy[k];
+                        int nx = j + dx[k];
+
+                        if (ny >= 0 && ny < N && nx >= 0 && nx < M && map[ny][nx] == -1) {
+                            count++;
+                        }
+                        if (count >= 2) {
+                            map[i][j] = 0;
                         }
                     }
-                    if (airContact >= 2) {
-                        grid[i][j] = 0; 
-                    }
                 }
-            }
-        }
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (grid[i][j] == -1) grid[i][j] = 0;
             }
         }
     }
 
-    static boolean hasCheeseLeft() {
+    private static boolean hasCheese() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (grid[i][j] == 1) return true;
+                if (map[i][j] == 1) {
+                    return true;
+                }
             }
         }
         return false;
