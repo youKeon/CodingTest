c2f8c804d81a9623f8de00c8555609a34884b4f9
youKeon <yukeon97@gmail.com>
2024-08-24 16:45:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 252 ms, Memory: 27852 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -2,48 +2,39 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int n;
-    static int[][] map, dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
 
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        dp = new int[n][n];
-
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int len = st.countTokens();
-            for (int j = 0; j < len; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j <= i; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        System.out.print(solution());
+        System.out.println(solution(n, arr));
     }
 
-    private static int solution() {
-        int ans = Integer.MIN_VALUE;
-        dp[0][0] = map[0][0];
-        if (n == 1) return dp[0][0];
-        dp[1][0] = dp[0][0] + map[1][0];
-        dp[1][1] = dp[0][0] + map[1][1];
-        if (n == 2) return Math.max(dp[1][0], dp[1][1]);
-        
-        for (int i = 2; i < n; i++) {
-            dp[i][0] = dp[i - 1][0] + map[i][0];
-            dp[i][i] = dp[i - 1][i - 1] + map[i][i];
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + map[i][j];
-            }
-        }
+    private static long solution(int n, int[][] arr) {
+        if (n == 1) return arr[0][0];
 
-        for (int[] i : dp) {
-            for (int j : i) {
-                ans = Math.max(ans, j);
+        long result = Long.MIN_VALUE;
+        long[][] dp = new long[n][n];
+        dp[0][0] = arr[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + arr[i][0];
+            dp[i][i] = dp[i - 1][i - 1] + arr[i][i];
+            result = Math.max(result, Math.max(dp[i][0], dp[i][i]));
+
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+                result = Math.max(dp[i][j], result);
             }
         }
-        return ans;
+        return result;
     }
 }
