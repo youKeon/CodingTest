efe6a6ca04f45d727777f3ddaf1fcd8e96765e5b
youKeon <yukeon97@gmail.com>
2024-09-24 20:14:09 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18084 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,15 +6,22 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[1] = 0;
+        System.out.print(solution(n));
+    }
 
-        for (int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+    private static int solution(int n) {
+        if (n == 1) return 0;
+        if (n == 2 || n == 3) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[2] = dp[3] = 1;
+
+        for (int i = 4; i <= n; i++) {
+            if (i % 6 == 0) dp[i] = Math.min(dp[i - 1], Math.min(dp[i / 3], dp[i / 2])) + 1;
+            else if (i % 2 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 2]) + 1;
+            else if (i % 3 == 0) dp[i] = Math.min(dp[i - 1], dp[i / 3]) + 1;
+            else dp[i] = dp[i - 1] + 1;
         }
-        System.out.print(dp[n]);
+        return dp[n];
     }
 }
